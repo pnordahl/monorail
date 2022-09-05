53ee89c718c91ef91a3401911fcc58e413957f53
pnordahl <plnordahl@gmail.com>
2022-09-05 08:25:17 -0500

    script and readme tweaks

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ fi
 
 # compile `monorail` and deposit it in the desired location
 cargo build --release
+echo "* Copying monorail to ${location}"
 cp target/release/monorail "$location"
 
 # set executable bit and install ext components to the desired location
+echo "* Copying monorail-bash to ${location}"
 chmod +x ext/bash/monorail-bash.sh
 cp ext/bash/monorail-bash.sh "${location}/monorail-bash"
